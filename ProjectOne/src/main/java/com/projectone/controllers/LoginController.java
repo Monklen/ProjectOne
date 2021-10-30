aadb634228c24547f08877701d07a68a10725ebe
Monklen <62600844+Monklen@users.noreply.github.com>
2021-10-29 22:58:50 -0600

    Add files via upload

--- ProjectOne/src/main/java/com/projectone/controllers/LoginController.java
+++ ProjectOne/src/main/java/com/projectone/controllers/LoginController.java
@@ -1,12 +1,16 @@
 package com.projectone.controllers;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.projectone.dao.UserDao;
+import com.projectone.models.User;
 import com.projectone.services.AccountService;
 
 public class LoginController {
@@ -19,6 +23,39 @@ public class LoginController {
 		//To read in stringified Json data from a post request is more complicated that reading form data
 		StringBuilder builder = new StringBuilder();
 		
+		//this buffered reader will read json data line by line
+		BufferedReader reader = req.getReader();
+		
+		String line;
+		while((line = reader.readLine()) != null) {
+			builder.append(line);
+			builder.append(System.lineSeparator());
+		}
+		
+		String data = builder.toString();
+		System.out.println(data);
+		
+		ObjectMapper mapper = new ObjectMapper();
+		JsonNode parsedObj = mapper.readTree(data);
+		
+		String username = parsedObj.get("username").asText();
+		String password = parsedObj.get("password").asText();
+		
+		try {
+			System.out.println("in the post handler");
+			User u = aServ.login(username, password);
+			System.out.println(u);
+			//keep track of if a user is signed in by storing their id in the session
+			req.getSession().setAttribute("id", u.getId());
+			res.setStatus(200);
+			res.getWriter().write(new ObjectMapper().writeValueAsString(u));
+		}
+		catch(Exception e) {
+			
+			res.setStatus(403);
+			res.getWriter().println("username or password is incorrect");
+			
+		}
 	}
 
 }
--- ProjectOne/src/main/java/com/projectone/servlets/Dispatcher.java
+++ ProjectOne/src/main/java/com/projectone/servlets/Dispatcher.java
@@ -6,11 +6,18 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.projectone.controllers.LoginController;
 
 public class Dispatcher {
 	
 	public static void process(HttpServletRequest req, HttpServletResponse res) throws JsonProcessingException, IOException{
 		System.out.println("In the servlet dispatcher with URI: " + req.getRequestURI());
+		
+		switch(req.getRequestURI()) {
+			case "/ProjectOne/api/login":
+				LoginController.login(req, res);
+				break;
+		}
 	}
 
 }
